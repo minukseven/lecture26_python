2c4d9135a76309f0cc21e337cb2173e21225b084
minukseven <minukseven@gmail.com>
2026-05-15 15:58:26 +0900

    Add files via upload

--- hangman_console.py
+++ hangman_console.py
@@ -1,31 +1,34 @@
-   
 from hangman import Hangman
 
-print()
-print('============= Hangman ============')
-word_list = ['apple', 'banana', 'man' 'woman', 'tomato']
+word_list = ["apple", "banana", "man", "woman", "tomato"]
 
-# 단어 선택
+print()
+print("========== Hangman ==========")
+
 hangman = Hangman(word_list)
-print(f'{hangman.display_word} ({len(hangman.word)}글자)')
+print(f"{hangman.display_word} {len(hangman.word)}글자")
 
 while True:
     # 알파벳 입력
-    letter = input('>> 알바벳 입력 : ')
+    letter = input(">> 알파벳 입력 : ")
+    if not letter.isalpha():
+        print("알파벳을 입력하세요.")
+        continue
 
     # 정답 확인
     result = hangman.check_letter(letter)
     if result == Hangman.RIGHT:
-        print(f'정답 : {hangman.display_word}')
+        print(f"정답 : {hangman.display_word}")
     elif result == Hangman.WRONG:
-        print(f'오답 : {hangman.num_try}회 시도')
-        
+        print(f"오답 : {hangman.num_try}회 시도")
+    else:
+        print(hangman.error_status)
 
     # 승패 확인
     result = hangman.is_win()
     if result == Hangman.WIN:
-        print(f'Yon win ~~~~ !!!! : {hangman.num_try}회 시도')
+        print(f"You win! : {hangman.num_try}회 틀림")
         break
-    elif result == Hangman.LOOSE:
-        print(f'Yon loose ~~~~!!!! : {hangman.word}')
+    elif result == Hangman.LOSE:
+        print(f"You lose! : {hangman.word}")
         break
