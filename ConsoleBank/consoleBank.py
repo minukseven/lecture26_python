6e69a3d4192ce0d23cb7cea7128ba66a24e04d31
minukseven <minukseven@gmail.com>
2026-06-01 19:32:38 +0900

    Add files via upload

--- ConsoleBank/consoleBank.py
+++ ConsoleBank/consoleBank.py
@@ -6,12 +6,12 @@ from Account.account_dao import AccountDAO
 from Account.account_service import AccountService
 
 class ConsoleBank:
-    start_menu = ['종료', '로그인', '회원가입']
-    banking_menu = []
-    member_myinfo_menu = []
-    admin_menu = []
-    admin_account_menu = []
-    admin_member_menu = []
+    start_menu         = ['종료', '로그인', '회원가입']
+    banking_menu       = ['종료', '잔액조회', '입금', '출금', '이체', '내정보']
+    member_myinfo_menu = ['뒤로', '개인정보 수정', '비밀번호 변경', '회원탈퇴']
+    admin_menu         = ['종료', '계좌관리', '회원관리']
+    admin_account_menu = ['뒤로', '전체계좌조회', '계좌검색', '계좌삭제']
+    admin_member_menu  = ['뒤로', '전체회원조회', '회원검색', '회원삭제']
 
     def __init__(self):
         self.msv = MemberService(MemberDAO())
@@ -20,12 +20,10 @@ class ConsoleBank:
     def main(self):
         self.show_welcome()
         while True:
-            menu = self.select_menu(ConsoleBank.select_menu)
+            menu = self.select_menu(ConsoleBank.start_menu)  # ← 버그 수정
             if menu == 0: break
-            elif menu == 1:
-                pass
-            elif menu == 2:
-                pass
+            elif menu == 1: pass   # 추후 login() 연결
+            elif menu == 2: pass   # 추후 register() 연결
         self.say_goodbye()
 
     def show_welcome(self):
@@ -35,7 +33,17 @@ class ConsoleBank:
         print('>> Hyejeong Console Bank를 이용해 주셔서 감사합니다.')
 
     def select_menu(self, menu_list):
-        return 0
+        print()
+        for i, item in enumerate(menu_list):
+            print(f'  {i}. {item}')
+        while True:
+            try:
+                choice = int(input(f'메뉴 선택 (0~{len(menu_list)-1}): '))
+                if 0 <= choice < len(menu_list):
+                    return choice
+                print(f'  >> 0~{len(menu_list)-1} 사이의 번호를 입력하세요.')
+            except ValueError:
+                print('  >> 숫자를 입력하세요.')
 
 if __name__ == '__main__':
     app = ConsoleBank()
